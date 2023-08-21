6a4487c97760e5e52aca0358afc9ed608877920e
程同学 <zenos.chen@foxmail.com>
2023-08-21 08:50:09 +0800

    feature: add server in local

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+/** 创建一个路由，总是返回dist文件夹下面的index.html */
+const router = express.Router();
+router.use(express.static(path.resolve(__dirname, "../dist")));
+router.get("/", (req, res) => {
+	const filePath = path.resolve(__dirname, "../dist/index.html");
+	res.sendFile(filePath, { Headers: { "Content-Type": "text/html" } });
+});
+
+const router2 = express.Router();
+router2.get("/getUser", (req, res) => {
+	res.end(JSON.stringify({ name: "zhangsan" }));
+});
+
+/** 启动服务器，监听express */
+const app = express();
+app.use(router);
+app.use("/fetch", router2);
+app.listen(3000, () => {
+	console.log("server is running at port 3000");
+});
